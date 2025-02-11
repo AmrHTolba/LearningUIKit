44f90efe3852e4bf9d4b926293b9a8112d5a80d4
AmrFiqi <amr.h.elfiqi@gmail.com>
2025-02-11 20:08:48 +0200

    Linear

--- LearningUIKit/FirstScreen.swift
+++ LearningUIKit/FirstScreen.swift
@@ -10,17 +10,50 @@ import UIKit
 class FirstScreen: UIViewController {
     // MARK: - Properties
     
-
+    let nextButton = UIButton()
     
     // MARK: - Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .systemRed
-        // Do any additional setup after loading the view.
+        applyGradientBackground(to: view)
+        setupButton()
     }
     
-    
+    func setupButton() {
+        view.addSubview(nextButton)
+        
+        nextButton.configuration = .filled()
+        nextButton.configuration?.baseBackgroundColor = UIColor(red: 230/255, green: 230/255, blue: 250/255, alpha: 1.0)
+        nextButton.configuration?.title = "Next"
+        nextButton.setTitleColor(UIColor(red: 134/255, green: 96/255, blue: 142/255, alpha: 1.0), for: .normal)
+        nextButton.layer.cornerRadius = 10
+        
+        nextButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            nextButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            nextButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            nextButton.widthAnchor.constraint(equalToConstant: 200),
+            nextButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+        
+    }
+    // Background LinearGradient effect
+    func applyGradientBackground(to view: UIView) {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        
+        // Define pastel lavender color scheme
+        let color1 = UIColor(red: 230/255, green: 230/255, blue: 250/255, alpha: 1.0).cgColor  // Lavender
+        let color2 = UIColor(red: 216/255, green: 191/255, blue: 216/255, alpha: 1.0).cgColor  // Thistle
+        let color3 = UIColor(red: 250/255, green: 240/255, blue: 255/255, alpha: 1.0).cgColor  // Lavender Blush
+        
+        gradientLayer.colors = [color1, color2, color3]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)  // Top-left
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)    // Bottom-right
+        
+        view.layer.insertSublayer(gradientLayer, at: 0)
+    }
 
 
 }
