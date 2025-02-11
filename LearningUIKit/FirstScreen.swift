643b43592d23798920480db88cbb55611f0eaa69
AmrFiqi <amr.h.elfiqi@gmail.com>
2025-02-12 00:55:30 +0200

    Minor UI Tweaks regarding the title

--- LearningUIKit/FirstScreen.swift
+++ LearningUIKit/FirstScreen.swift
@@ -19,15 +19,19 @@ class FirstScreen: UIViewController {
         view.applyGradientBackground()
         setupButton()
         navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.largeTitleTextAttributes = [
+                .foregroundColor: UIColor(red: 92/255, green: 64/255, blue: 120/255, alpha: 1.0),
+                .font: UIFont.systemFont(ofSize: 50, weight: .bold) // Optional: Customize font
+            ]
     }
     
     func setupButton() {
         view.addSubview(nextButton)
         
         nextButton.configuration = .filled()
-        nextButton.configuration?.baseBackgroundColor = UIColor(red: 230/255, green: 230/255, blue: 250/255, alpha: 1.0)
+        nextButton.configuration?.baseBackgroundColor = UIColor(red: 200/255, green: 180/255, blue: 225/255, alpha: 1.0)
         nextButton.configuration?.title = "Next"
-        nextButton.setTitleColor(UIColor(red: 134/255, green: 96/255, blue: 142/255, alpha: 1.0), for: .normal)
+        nextButton.setTitleColor(UIColor(red: 60/255, green: 40/255, blue: 90/255, alpha: 1.0), for: .normal)
         nextButton.layer.cornerRadius = 10
         
         nextButton.translatesAutoresizingMaskIntoConstraints = false
