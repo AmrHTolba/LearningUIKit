96064cb93989372c168ad3b57bb78c641007b5a2
AmrFiqi <amr.h.elfiqi@gmail.com>
2025-02-12 18:14:00 +0200

    Building Programmatic tableview

--- LearningUIKit/FirstScreen.swift
+++ LearningUIKit/FirstScreen.swift
@@ -9,15 +9,42 @@ import UIKit
 
 class FirstScreen: UIViewController {
     // MARK: - Properties
+    private let images: [UIImage] = [
+        UIImage(named: "1")!,
+        UIImage(named: "2")!,
+        UIImage(named: "3")!,
+        UIImage(named: "4")!,
+        UIImage(named: "5")!
+    ]
+    
+    // MARK: - UI Components
+    private var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.backgroundColor = .clear
+        tableView.allowsSelection  = true
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        return tableView
+    }()
     
     let nextButton = UIButton()
     
-    // MARK: - Methods
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    // MARK: - Setup UI
+    
+    private func setupUI() {
         title = "First Screen"
         view.applyGradientBackground()
         setupButton()
+        setupTableView()
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationController?.navigationBar.largeTitleTextAttributes = [
                 .foregroundColor: UIColor(red: 92/255, green: 64/255, blue: 120/255, alpha: 1.0),
@@ -25,8 +52,21 @@ class FirstScreen: UIViewController {
             ]
     }
     
+    func setupTableView() {
+        view.addSubview(tableView)
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        ])
+    }
+    
     func setupButton() {
-        view.addSubview(nextButton)
+        //view.addSubview(nextButton)
         
         nextButton.configuration = .filled()
         nextButton.configuration?.baseBackgroundColor = UIColor(red: 200/255, green: 180/255, blue: 225/255, alpha: 1.0)
@@ -38,13 +78,14 @@ class FirstScreen: UIViewController {
         
         nextButton.addTarget(self, action: #selector(goToNextScreen), for: .touchUpInside)
         
-        NSLayoutConstraint.activate([
+        /* NSLayoutConstraint.activate([
             nextButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             nextButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             nextButton.widthAnchor.constraint(equalToConstant: 200),
             nextButton.heightAnchor.constraint(equalToConstant: 50)
         ])
         
+        */
     }
 
     
@@ -53,6 +94,19 @@ class FirstScreen: UIViewController {
         navigationController?.pushViewController(nextScreen, animated: true)
     }
 
-
+    
 }
 
+extension FirstScreen: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return images.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = UIColor(red: 230/255, green: 220/255, blue: 250/255, alpha: 1.0)
+        cell.textLabel?.text = "\(indexPath.row.description)"
+        return cell
+    }
+}
