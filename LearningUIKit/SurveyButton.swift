0183f7e9f7ec849e5f26f3b1352d8b0509fb5a9d
AmrFiqi <amr.h.elfiqi@gmail.com>
2025-02-13 11:17:07 +0200

    Created a setup for a button

---
+++ LearningUIKit/SurveyButton.swift
@@ -0,0 +1,28 @@
+//
+//  SurveyButton.swift
+//  LearningUIKit
+//
+//  Created by Amr El-Fiqi on 13/02/2025.
+//
+
+import UIKit
+
+class SurveyButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+    }
+    
+    func setupButton() {
+        self.backgroundColor = UIColor(red: 60/255, green: 40/255, blue: 90/255, alpha: 1.0)
+        self.setTitleColor(.white, for: .normal)
+        self.layer.cornerRadius = 10
+        self.titleLabel?.font = UIFont(name: "AvenirNext-Medium", size: 20)
+    }
+}
