43f185e986fc1712d4fcb9dd655191ec3d75aebe
AmrFiqi <amr.h.elfiqi@gmail.com>
2025-02-13 11:28:39 +0200

    Calling setup func in the init

--- LearningUIKit/SurveyButton.swift
+++ LearningUIKit/SurveyButton.swift
@@ -11,12 +11,12 @@ class SurveyButton: UIButton {
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
+        setupButton()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
-        
+        setupButton()
     }
     
     func setupButton() {
